b7f816a64a2423c0a00e03ccc9db5e0f547270fe
benpullman <bp2@icloud.com>
2020-12-01 17:11:22 -0800

    initial commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+-include ../Makefile.credentials
+include ../Makefile.deploytemplate
+
+TOOL_FOLDER_NAME=generate_sqlite_result
+WORKFLOW_VERSION=0.1
+WORKFLOW_LABEL="SQLite Result Generation"
+WORKFLOW_DESCRIPTION="This is a script to convert tsv or csv files into SQLite files."
---
+++ tools/generate_sqlite_result/convert_tsv_to_sqlite.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import subprocess
+import sys
+
+def result_file_name(result_file, result_view_name):
+    return f'{result_view_name}-main_{result_file.with_suffix(".db").name}'
+
+def run_mysql_convert(mysql_binary, result_file, result_view_name, output_folder):
+    seperator = []
+    if 'tsv' in result_file.name:
+        seperator = ['-s', "$'\t'"]
+    cmd = [
+        mysql_binary
+    ] + seperator + [
+        '-t', 'Result',
+        result_file,
+        output_folder.joinpath(result_file_name(result_file, result_view_name))
+    ]
+    print(' '.join([str(c) for c in cmd]))
+    subprocess.run(cmd)
+
+def main():
+
+    mysql_binary = Path(sys.argv[1])
+    output_folder = Path(sys.argv[2])
+    outputs = sys.argv[3:]
+
+    if len(outputs) % 1 == 1:
+        raise Exception("Cannot have odd number of inputs after mysql_binary")
+
+    grouped_outputs = [(outputs[2*i][1:],Path(outputs[2*i+1])) for i in range(int(len(outputs)/2))]
+
+    for (result_view_name, result_file) in grouped_outputs:
+        run_mysql_convert(mysql_binary, result_file, result_view_name, output_folder)
+
+if __name__ == '__main__':
+    main()
