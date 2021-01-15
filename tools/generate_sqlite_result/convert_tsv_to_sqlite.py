fb5e03e0d17e40f2f9e169659a37698e45ae072d
benpullman <bp2@icloud.com>
2021-01-15 09:08:03 -0800

    update to alter SQLite file and not rename

--- tools/generate_sqlite_result/convert_tsv_to_sqlite.py
+++ tools/generate_sqlite_result/convert_tsv_to_sqlite.py
@@ -1,23 +1,33 @@
 from pathlib import Path
 import subprocess
 import sys
+import shutil
+import sqlite3
 
 def result_file_name(result_file, result_view_name):
     return f'{result_view_name}-main_{result_file.with_suffix(".db").name}'
 
+def rename_table(db, table_name):
+    renameTable = "ALTER TABLE {} RENAME TO Result".format(table_name)
+    print(renameTable)
+    connection  = sqlite3.connect(db)
+    cursor      = connection.cursor()
+    cursor.execute(renameTable)
+
 def run_mysql_convert(mysql_binary, result_file, result_view_name, output_folder):
-    result_file_alias = Path('Result').with_suffix('.{}'.format(result_file.name.replace('.','_')))
-    result_file_alias.symlink_to(result_file)
+    db = output_folder.joinpath(result_file_name(result_file, result_view_name))
     seperator = []
     if 'tsv' in result_file.name:
         seperator = ['--delimiter', '\t']
     cmd = [
         mysql_binary
     ] + seperator + [
-        '--file', result_file_alias,
-        '--output', output_folder.joinpath(result_file_name(result_file, result_view_name))
+        '--file', result_file,
+        '--output', db
     ]
+    print(cmd)
     p = subprocess.run(cmd)
+    rename_table(db,result_file.stem)
 
 def main():
 
