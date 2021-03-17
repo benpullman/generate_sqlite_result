ca25b8789f2bfe73ce6b007107ac2634d037d1cc
benpullman <bp2@icloud.com>
2021-03-16 17:05:38 -0700

    final updates to include indexes

--- tools/generate_sqlite_result/convert_tsv_to_sqlite.py
+++ tools/generate_sqlite_result/convert_tsv_to_sqlite.py
@@ -8,25 +8,27 @@ def result_file_name(result_file, result_view_name):
 
 def rename_table(db, table_name, index_columns):
     renameTable = 'ALTER TABLE "{}" RENAME TO Result'.format(table_name)
+    print(renameTable)
     indexOperations = []
     for index in index_columns:
-        index_name = index.replace('+','_')
-        index_columns = index.replace('+',',')
-        indexOperations.append('CREATE UNIQUE INDEX {} ON Result ({})'.format(index_name, index_columns))
-    print(renameTable)
+        index_name = index.replace(' ','_').replace('+','_')
+        index_columns = ','.join(['"{}"'.format(i) for i in index.split('+') if i != ''])
+        indexOperations.append('CREATE INDEX index_{} ON Result ({})'.format(index_name, index_columns))
     connection  = sqlite3.connect(db)
     cursor      = connection.cursor()
     cursor.execute(renameTable)
     for indexOperation in indexOperations:
+        print(indexOperation)
         cursor.execute(indexOperation)
 
 def run_mysql_convert(result_file, result_view_name, index_columns, output_folder):
     db = output_folder.joinpath(result_file_name(result_file, result_view_name))
     # all the usual options are supported
-    delimiter = ','
-    if 'tsv' in result_file.name:
-        delimiter = '\t'
+    delimiter = '\t'
+    if 'csv' in result_file.name:
+        delimiter = ','
     options = csv_to_sqlite.CsvOptions(delimiter=delimiter)
+    print([str(result_file)])
     csv_to_sqlite.write_csv([str(result_file)], str(db), options)
     rename_table(db,result_file.stem, index_columns)
 
@@ -38,7 +40,7 @@ def main():
     if len(outputs) % 1 == 1:
         raise Exception("All outputs must be paired (-result_view_name result_view_file:index1,index2)")
 
-    grouped_outputs = [(outputs[2*i][1:],Path(outputs[2*i+1].split(':')[0]),[s for s in outputs[2*i+1].split(':')[1].split(',') if s != '']) for i in range(int(len(outputs)/2))]
+    grouped_outputs = [(outputs[2*i].split(':')[0][1:],Path(outputs[2*i+1]),[s for s in outputs[2*i].split(':')[1].split(',') if s != '']) for i in range(int(len(outputs)/2))]
 
     for (result_view_name, result_file, index_columns) in grouped_outputs:
         run_mysql_convert(result_file, result_view_name, index_columns, output_folder)
